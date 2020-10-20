e84799e2dc94e2795cf88913b793dc2fe56c11da
vvictorious <vvictorious30@gmail.com>
2020-10-20 14:04:24 -0600

    built my first route and it is working on postman

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact keeper API'}))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
