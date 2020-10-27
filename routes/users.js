af1f97699c9af32cd274b82ee3b5f1e8b4cbbed9
vvictorious <vvictorious30@gmail.com>
2020-10-27 12:48:41 -0600

    adding a user to the db successfully

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/User')
 
@@ -14,12 +15,31 @@ router.post('/', [
     check('password', 'Please enter a password with 6 or more characters').isLength({
         min: 6
     })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ erros: errors.array() })
     }
-    res.send('passed')
+    const { name, email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email })
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' })
+        }
+        user = new User({
+            name,
+            email,
+            password
+        })
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
+        await user.save()
+        res.send('user saved')
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
 })
 
 module.exports = router
