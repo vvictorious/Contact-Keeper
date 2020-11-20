54224ffd9af4fce3057ce1a66d11a029d9b8c943
vvictorious <vvictorious30@gmail.com>
2020-11-20 14:59:57 -0700

    getting and rendering the data from the intial state in the contactItem component in a nice manner

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const ContactItem = ({ contact }) => {
     return (
         <div className='card bg-light'>
             <h3 className='text-primary text-left'>{contact.name}{' '}
-                <span className={'badge ' + (contact.type === 'professional' ? 'badge-success' : 'badge-primary')}>{contact.type.charAt(0).toUpperCase() + contact.type.slice(1)}</span>
+                <span 
+                    style={{ float: 'right'}} 
+                    className={'badge ' + (contact.type === 'professional' ? 'badge-success' : 'badge-primary')}>
+                        {contact.type.charAt(0).toUpperCase() + contact.type.slice(1)}
+                </span>
             </h3>
+            <ul className='list'>
+                {contact.email && (<li>
+                    <i className='fas fa-envelope-open env-phone-icons'></i>{contact.email}
+                </li>)}
+                {contact.phone && (<li>
+                    <i className='fas fa-phone env-phone-icons'></i>{contact.phone}
+                </li>)}                
+            </ul>
+            <p>
+                <button className='btn btn-dark btn-sm'>Edit</button>
+                <button className='btn btn-danger btn-sm'>Delete</button>
+            </p>
         </div>
     )
+}
+
+ContactItem.propTypes = {
+    contact: PropTypes.object.isRequired
 }
 
 export default ContactItem
