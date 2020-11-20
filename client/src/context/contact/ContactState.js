a8f462a4792b05325e7f3b8504ec1e0854952bd7
vvictorious <vvictorious30@gmail.com>
2020-11-20 09:27:35 -0700

    context is just about complete with hard coded data

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,3 +2,45 @@ import React, { useReducer } from 'react'
 import uuid from 'uuid'
 import ContactContext from './contactContext'
 import ContactReducer from './contactReducer'
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types'
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'John Appleseed',
+                email: 'john@gmail.com',
+                phone: '7202022020',
+                type: 'personal'
+            },
+            {
+                id: 2,
+                name: 'Will Smith',
+                email: 'will@gmail.com',
+                phone: '3030303030',
+                type: 'personal'
+            },       
+            {
+                id: 3,
+                name: 'Big Kahuna',
+                email: 'kahuna@gmail.com',
+                phone: '7202022020',
+                type: 'personal'
+            },                 
+        ]
+    }
+
+    const [state, dispatch] = useReducer(ContactReducer, initialState)
+
+    //actions will be below
+
+}
