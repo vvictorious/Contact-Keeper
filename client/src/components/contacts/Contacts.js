e09949b2765d71d26376cdd4e206e8875aedd858
vvictorious <vvictorious30@gmail.com>
2020-11-24 15:53:00 -0700

    filtering is working (hurray) probably the most complicated feature in this app

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -6,13 +6,19 @@ const Contacts = () => {
 
     const contactContext = useContext(ContactContext)
 
-    const { contacts } = contactContext
+    const { contacts, filtered } = contactContext
+
+    if (contacts.length === 0) {
+        return <h4>Please add a contact</h4>
+    }
 
     return (
         <Fragment>
-            {contacts.map( contact =>
-                <ContactItem contact={contact} key={contact.id} />
-            )}
+            {filtered !== null ? 
+                filtered.map(contact => (<ContactItem contact={contact} key={contact.id} />)) 
+            :
+                contacts.map( contact => <ContactItem contact={contact} key={contact.id} />)
+            }
         </Fragment>
     )
 }
