59e4d34dc19eb78a8219e964707e0057007722ca
vvictorious <vvictorious30@gmail.com>
2020-11-25 09:32:12 -0700

    register is being rendered when user goes to that route

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export const Register = () => {
+const Register = () => {
 
     const [user, setUser] = useState({
         name: '',
@@ -11,17 +11,39 @@ export const Register = () => {
 
     const { name, email, password, password2 } = user
 
+    const onChange = e => setUser({ ...user, [e.target.name]: e.target.value})
+
+    const onSubmit = e => {
+        e.preventDefaault()
+        console.log('Register Submit')
+    }
+
     return (
         <div className='form-container'>
             <h1>
-                Account<span className='text-primary'>Account Register</span>
+                Account<span className='text-primary'> Register</span>
             </h1>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className='form-group'>
                     <label htmlFor='name'>Name</label>
                     <input type='text' name='name' value={name} onChange={onChange} />
                 </div>
+                <div className='form-group'>
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' name='email' value={email} onChange={onChange} />
+                </div>      
+                <div className='form-group'>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' name='password' value={password} onChange={onChange} />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password2'>Confirm Password</label>
+                    <input type='password' name='password2' value={password2} onChange={onChange} />
+                </div> 
+                <input type='submit' value='Register' className='btn btn-primary btn-block' />
             </form>
         </div>
     )
 }
+
+export default Register
