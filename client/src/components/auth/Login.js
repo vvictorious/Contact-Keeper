5710a7ae7764b780e001c92f6df93e3392b3bcd1
vvictorious <vvictorious30@gmail.com>
2020-11-28 09:13:56 -0700

    had to change the value for the login button

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -57,7 +57,7 @@ const Login = (props) => {
                     <label htmlFor='password'>Password</label>
                     <input type='password' name='password' value={password} onChange={onChange} />
                 </div>
-                <input type='submit' value='Register' className='btn btn-primary btn-block' />
+                <input type='submit' value='Login' className='btn btn-primary btn-block' />
             </form>
         </div>
     )
